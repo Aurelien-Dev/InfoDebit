fe9469cebd92c175d6fafa45d844d20a2acdd683
Cretaine Aurélien (ext) <U0373@ADPROD.RAMQ.GOV>
2019-06-26 09:57:30 -0400

    Modification des variables d'environnement

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-var env = process.env.NODE_ENV || "development";
+var env = process.env.NODE_ENV || 'dev';
 
-var config = require('./configs/config.json')['production'];
+var config = require('./configs/config.json')['prod'];
 config.infoDebit.rivieres = require('./configs/rivieres.json');
 
 module.exports = config;
